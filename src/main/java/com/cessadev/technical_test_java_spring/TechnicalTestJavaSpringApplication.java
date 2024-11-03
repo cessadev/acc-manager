a6a9fcb7c6f58255c48a79ae0c9888b4fe8734a7
cessadev <cesarurb.dev@gmail.com>
2024-11-03 10:34:22 -0500

    First commit

---
+++ src/main/java/com/cessadev/technical_test_java_spring/TechnicalTestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cessadev.technical_test_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cessadev/technical_test_java_spring/TechnicalTestJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cessadev.technical_test_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
